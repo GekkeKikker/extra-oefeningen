529291ae93e0ecf60acabf5e3ff664eed2ea6cb3
GekkeKikker <15897712+GekkeKikker@users.noreply.github.com>
2024-10-15 10:15:49 +0200

    opdracht 1 deel 3 oplossing

---
+++ alle-kennis-opdracht-1-deel-3.js
@@ -0,0 +1,16 @@
+function berekenTotaal(aantalLussen) {
+  let totaal = 0;
+
+  let i = 0;
+  while (i < aantalLussen) {
+    totaal += i; // Zelfde als: totaal = totaal + i;
+    i++;
+  }
+  return totaal;
+}
+
+console.log("Het totaal is: ", berekenTotaal(10));
+console.log("Het totaal is: ", berekenTotaal(25));
+console.log("Het totaal is: ", berekenTotaal(100));
+console.log("Het totaal is: ", berekenTotaal(666));
+console.log("Het totaal is: ", berekenTotaal(10000));
